7958a58ebc8166e1b8b92e4091fb2420f364a4bb
simrin051 <simrinjoshi@yahoo.com>
2021-09-17 01:09:29 +0530

    Resume dev done

---
+++ main.js
@@ -0,0 +1,23 @@
+
+ function clickedNav()
+ {
+    var crossIcon=true;
+    document.getElementById("navMobIcon").style.display="none";
+    document.getElementById("navCrossIcon").style.display="block";
+    document.getElementById("navCrossIcon").style.visibility="visible";
+ }
+
+
+
+ function clickedNavCrossIcon()
+ {
+    console.log("clicked Nav Cross Icon");
+    document.getElementById("navMobIcon").style.display="block";
+    document.getElementById("navMobIcon").style.visibility="visible";
+    document.getElementById("navCrossIcon").style.display="none";
+ }
+
+ document.getElementById('navCrossIcon').addEventListener("click",clickedNavCrossIcon);
+ console.log( document.getElementById('navMobIcon')+"Nav value");
+ document.getElementById('navMobIcon').addEventListener("click",clickedNav);
+
