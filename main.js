cb9de336c21efe0d78af8b708c261ca21f24c5ec
simrin051 <simrinjoshi@yahoo.com>
2021-09-30 21:01:55 +0530

    Removed bootstrap and formatted

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
+function clickedNav() {
+  var crossIcon = true;
+  document.getElementById("navMobIcon").style.display = "none";
+  document.getElementById("navCrossIcon").style.display = "block";
+  document.getElementById("navCrossIcon").style.visibility = "visible";
+}
 
- function clickedNav()
- {
-    var crossIcon=true;
-    document.getElementById("navMobIcon").style.display="none";
-    document.getElementById("navCrossIcon").style.display="block";
-    document.getElementById("navCrossIcon").style.visibility="visible";
- }
+function clickedNavCrossIcon() {
+  console.log("clicked Nav Cross Icon");
+  document.getElementById("navMobIcon").style.display = "block";
+  document.getElementById("navMobIcon").style.visibility = "visible";
+  document.getElementById("navCrossIcon").style.display = "none";
+}
 
-
-
- function clickedNavCrossIcon()
- {
-    console.log("clicked Nav Cross Icon");
-    document.getElementById("navMobIcon").style.display="block";
-    document.getElementById("navMobIcon").style.visibility="visible";
-    document.getElementById("navCrossIcon").style.display="none";
- }
-
- document.getElementById('navCrossIcon').addEventListener("click",clickedNavCrossIcon);
- console.log( document.getElementById('navMobIcon')+"Nav value");
- document.getElementById('navMobIcon').addEventListener("click",clickedNav);
-
+document
+  .getElementById("navCrossIcon")
+  .addEventListener("click", clickedNavCrossIcon);
+console.log(document.getElementById("navMobIcon") + "Nav value");
+document.getElementById("navMobIcon").addEventListener("click", clickedNav);
